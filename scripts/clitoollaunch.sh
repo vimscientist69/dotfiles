fc37cef622298da22b7ac01f7f7ab640a011c59d
William <vimscientist69@gmail.com>
2025-02-09 01:49:05 +0200

    browser, discord and spotify

--- scripts/clitoollaunch.sh
+++ scripts/clitoollaunch.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 hydeScrDir="$(dirname "$(realpath "$(which wallpaper.sh)")")"
-terminalFileExplorer="${1:-yazi}"
+cliTool=$1
 
 # shellcheck disable=SC1091
 source "${hydeScrDir}/globalcontrol.sh"
 
-if pkg_installed $terminalFileExplorer; then
+if pkg_installed $cliTool; then
         term=$(grep -E '^\s*'"$term" "$HOME/.config/hypr/keybindings.conf" | cut -d '=' -f2 | xargs) # dumb search the config
         term=${TERMINAL:-$term}                                                                      # Use env var
         term=${SYSMONITOR_TERMINAL:-$term}                                                           # use the declared variable
-	pkill -x $terminalFileExplorer || $term $terminalFileExplorer &
+	pkill -x $cliTool || $term $cliTool &
 fi
