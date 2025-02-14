ed7b376381c0bc465b205b2d6e67ac9eea091a39
William Ferns <williamferns@Williams-MacBook-Pro.local>
2025-02-15 01:55:59 +0200

    git config, install obsidian, gh cli, move gitconfig, install lazygit

--- setup.sh
+++ setup.sh
@@ -1,9 +1,29 @@
+echo "Installing homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Moving keyboard layout to input sources..."
 sudo rm /Library/Keyboard\ Layouts/dvorak_prog_improved.keylayout
 sudo cp ./keyboard/dvorak_prog_improved.keylayout /Library/Keyboard\ Layouts
 
 if [ ! -d "$HOME/.config" ]; then
+	echo "creating `.config`..."
 	mkdir $HOME/.config
 fi
 
+echo "Moving tmux config to config..." 
 ln -s "$PWD/tmux/" "$HOME/.config/tmux"
 
+echo "Moving tmux config to home..." 
+ln -s "$PWD/gitconfig/" "$HOME/.gitconfig"
+
+echo "Installing Github CLI..."
+brew install gh
+
+echo "Authenticating with github cli..."
+gh auth login
+
+echo "Installing Obsidian..."
+brew install --cask obsidian
+
+echo "Installing Lazygit..."
+brew install jesseduffield/lazygit/lazygit
