6adb73c191dff04bcadd3591a4181b0b90f76436
William <vimscientist69@gmail.com>
2025-02-21 11:02:55 -0600

    feat: personal portfolio project

--- tmux/setup-projects-sessions.sh
+++ tmux/setup-projects-sessions.sh
@@ -17,4 +17,8 @@ echo "Setting up game project session..."
 tmux new-session -ds game -c ~/dev/game -n editor
 tmux new-window -d "cd /dev/game && lazygit"
 
+echo "Setting up portfolio project session..."
+tmux new-session -ds portfolio -c ~/dev/portfolio -n editor
+tmux new-window -d "cd /dev/portfolio && lazygit"
+
 echo "Setup all project sessions success! 🎊"
