2409690712c4b7679202cfffe7f4b1e0f7e3a1d3
William <vimscientist69@gmail.com>
2025-02-28 14:31:17 -0700

    feat(lspconfig.init,set): completion, snippets, lsp mappings

---
+++ nvim/lua/plugins/plugins.lua
@@ -0,0 +1,22 @@
+-- Small utility plugins 
+
+return {
+  {
+    "Wansmer/treesj",
+    keys = {
+      { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
+    },
+    opts = {
+      use_default_keymaps = false,
+      max_join_length = 150,
+    },
+
+  },
+  {
+    "monaqa/dial.nvim",
+    -- lazy-load on keys
+    -- mode is `n` by default. For more advanced options, check the section on key mappings
+    keys = { "<C-a>", { "<C-x>", mode = "n" } },
+  },
+}
+
---
+++ nvim/lua/set.lua
@@ -0,0 +1,8 @@
+vim.o.relativenumber = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.number = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.syntax="ON"
