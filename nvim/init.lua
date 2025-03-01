d5340b7f00e0a3b85cdec198b0b07ef9dffd82a1
William <vimscientist69@gmail.com>
2025-03-01 08:30:51 -0700

    feat(alacritty,init): improved appearance of terminal and nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,3 +28,6 @@ autocmd('LspAttach', {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'none' })
