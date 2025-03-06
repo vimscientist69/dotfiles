78a9b487a12ad95610691719d5f2454d18358960
William <vimscientist69@gmail.com>
2025-03-06 15:18:19 -0700

    clean

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -2,15 +2,16 @@ return {
 	{
 		"stevearc/conform.nvim",
 		opts = {
-			-- format_on_save = {
-			-- 	-- These options will be passed to conform.format()
-			-- 	timeout_ms = 500,
-			-- 	lsp_format = "fallback",
-			-- },
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
 			formatters_by_ft = {
 				lua = { "stylua" },
 				zsh = { "shfmt" },
 				sh = { "shfmt" },
+                swift = { "swiftformat" },
 				-- Conform will run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				-- You can customize some of the format options for the filetype (:help conform.format)
